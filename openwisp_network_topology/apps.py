2165f930b2f7e8df7fd7780ce348992c313f64de
Federico Capoano <nemesis@ninux.org>
2017-07-10 12:51:12 +0200

    [AppConfig] Simplified verbose_name for consistency

--- openwisp_network_topology/apps.py
+++ openwisp_network_topology/apps.py
@@ -1,8 +1,6 @@
-from django.utils.translation import ugettext_lazy as _
 from django_netjsongraph.apps import DjangoNetjsongraphConfig
 
 
 class OpenwispNetworkTopologyConfig(DjangoNetjsongraphConfig):
     name = 'openwisp_network_topology'
     label = 'openwisp_network_topology'
-    verbose_name = _('OpenWISP Network Topology')
