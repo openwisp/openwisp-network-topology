4440eca3818f804c7c9f2f4e008db303183394ef
Reza Juliandri <rezajuliandri20@gmail.com>
2018-12-09 23:38:18 +0700

    [migration] Added default operator permission #29
    Fixes #29

---
+++ openwisp_network_topology/migrations/0005_default_operator_permission.py
@@ -0,0 +1,58 @@
+from django.db import migrations
+from django.contrib.auth.models import Permission
+from django.contrib.auth.management import create_permissions
+
+def create_default_permissions(apps, schema_editor):
+    for app_config in apps.get_app_configs():
+        app_config.models_module = True
+        create_permissions(app_config, apps=apps, verbosity=0)
+        app_config.models_module = None
+
+
+def assign_permissions_to_groups(apps, schema_editor):
+    create_default_permissions(apps, schema_editor)
+    operators_and_admin_can_change = ['link', 'node']
+    operators_read_only_admins_manage = ['topology']
+    manage_operations = ['add', 'change', 'delete']
+    Group = apps.get_model('openwisp_users', 'Group')
+
+    try:
+        admin = Group.objects.get(name='Administrator')
+        operator = Group.objects.get(name='Operator')
+    # consider failures custom cases
+    # that do not have to be dealt with
+    except Group.DoesNotExist:
+        return
+
+    for model_name in operators_and_admin_can_change:
+        for operation in manage_operations:
+            permission = Permission.objects.get(
+                codename='{}_{}'.format(operation, model_name)
+            )
+            admin.permissions.add(permission.pk)
+            operator.permissions.add(permission.pk)
+    for model_name in operators_read_only_admins_manage:
+        try:
+            permission = Permission.objects.get(
+                codename="view_{}".format(model_name)
+            )
+            operator.permissions.add(permission.pk)
+        except Permission.DoesNotExist:
+            pass        
+        for operation in manage_operations:
+            admin.permissions.add(
+                Permission.objects.get(codename="{}_{}".format(operation, model_name)).pk
+            )
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('openwisp_users', '0004_default_groups'),
+        ('topology', '0004_fixed_target_link_set')
+    ]
+    operations = [
+        migrations.RunPython(
+            assign_permissions_to_groups,
+            reverse_code=migrations.RunPython.noop
+        )
+    ]
