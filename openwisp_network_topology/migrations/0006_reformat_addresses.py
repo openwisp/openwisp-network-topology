cc43893da44cafd0f4b6a31ef3361b7a93f9c977
Karol <2004karol@gmail.com>
2019-12-03 14:32:37 +0100

    [Node] Reformatted addresses by data migration #18
    Added data migration to reformat addresses
    Fixes #18

---
+++ openwisp_network_topology/migrations/0006_reformat_addresses.py
@@ -0,0 +1,27 @@
+from django.db import migrations
+
+def reformat_address_forward(apps, schema_editor):
+    Node = apps.get_model('topology', 'Node')
+    for node in Node.objects.all():
+        if not node.addresses.startswith(';'):
+            node.addresses = ';{0}'.format(node.addresses)
+            node.save()
+
+
+def reformat_address_backward(apps, schema_editor):
+    Fake_node_model = apps.get_model('topology', 'Node')
+    for node in Fake_node_model.objects.all():
+        if node.addresses.startswith(';'):
+            node.addresses = node.addresses[1:]
+            node.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('topology', '0005_default_operator_permission'),
+    ]
+
+    operations = [
+        migrations.RunPython(reformat_address_forward, reformat_address_backward)
+    ]
