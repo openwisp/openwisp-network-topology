c1d4af8ad11ea489759ff11e706963c108839f09
Federico Capoano <nemesis@ninux.org>
2020-01-09 19:27:57 +0100

    [tests] Updated openwisp-utils to 0.3.2

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import include, url
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-from openwisp_utils.admin_theme.admin import admin, openwisp_admin
-
-openwisp_admin()
 
 urlpatterns = [
     url(r'^', include('openwisp_network_topology.urls')),
