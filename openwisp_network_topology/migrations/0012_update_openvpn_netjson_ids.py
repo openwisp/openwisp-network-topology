ade7c4e4271853e6f16ec51f79a5d3f004a7f006
Federico Capoano <federico.capoano@gmail.com>
2020-07-10 17:08:36 -0500

    [change] Upgraded openvpn nodes to netdiff 0.9

---
+++ openwisp_network_topology/migrations/0012_update_openvpn_netjson_ids.py
@@ -0,0 +1,15 @@
+from django.db import migrations
+
+from . import migrate_openvpn_ids_0012
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('topology', '0011_fix_link_properties'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            migrate_openvpn_ids_0012, reverse_code=migrations.RunPython.noop
+        )
+    ]
--- openwisp_network_topology/migrations/__init__.py
+++ openwisp_network_topology/migrations/__init__.py
@@ -1,4 +1,5 @@
 # Manually written, used during migrations
+import swapper
 
 
 def migrate_addresses(apps, schema_editor, app='topology'):
@@ -9,4 +10,20 @@ def migrate_addresses(apps, schema_editor, app='topology'):
             addresses = addresses[1:]
         addresses = addresses[0:-1].split(';')
         node.addresses = addresses
+        node.save()
+
+
+def migrate_openvpn_ids_0012(apps, schema_editor, app='topology'):
+    Node = swapper.load_model('topology', 'Node')
+    queryset = Node.objects.filter(topology__parser='netdiff.OpenvpnParser')
+    for node in queryset.iterator():
+        address = node.properties.get('real_address')
+        common_name = node.label
+        port = node.properties.get('port')
+        netjson_id = f'{common_name},{address}'
+        if node.netjson_id == f'{address}:{port}':
+            if Node.objects.filter(addresses__contains=netjson_id).exists():
+                netjson_id = f'{netjson_id}:{port}'
+        node.addresses[0] = netjson_id
+        node.full_clean()
         node.save()
