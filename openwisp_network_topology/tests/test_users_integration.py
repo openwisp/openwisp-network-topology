5a8b3ddf2dc2724e3b683f9282e5fb0afa0421ca
Federico Capoano <federico.capoano@gmail.com>
2020-07-13 20:52:09 -0500

    [tests] Updated openwisp-users integration tests

--- openwisp_network_topology/tests/test_users_integration.py
+++ openwisp_network_topology/tests/test_users_integration.py
@@ -1,9 +1,12 @@
-from openwisp_users.tests.test_admin import TestUsersAdmin
+from openwisp_users.tests.test_admin import TestBasicUsersIntegration
 
 
-class TestUsersIntegration(TestUsersAdmin):
+class TestUsersIntegration(TestBasicUsersIntegration):
     """
     tests integration with openwisp_users
     """
 
     pass
+
+
+del TestBasicUsersIntegration
