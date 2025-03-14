04d92d0aedf05ec3f8d6f0498ba966b42f7adf7e
Federico Capoano <f.capoano@openwisp.io>
2025-03-14 17:02:46 -0300

    [tests] Updated selenium tests, CI, prettier, etc
    * [tests] Updated selenium tests
    * [qa] Reformatted JS/CSS with prettier
    * [ci] APT cache, disable man autoupdate, headless mode, show gecko log
    * [ci] Exclude selenium from repeated test runs
    * [tests] Updated test mesh data (uppercase macaddr)
       OpenWISP-Controller now enforces uppercase mac addresses

--- openwisp_network_topology/static/netjsongraph/js/visualize.js
+++ openwisp_network_topology/static/netjsongraph/js/visualize.js
@@ -1,68 +1,68 @@
 django.jQuery(function ($) {
-    'use strict';
-    var overlay = $('.djnjg-overlay'),
-        body = $('body'),
-        inner = overlay.find('.inner'),
-        visualizeUrl = $('.visualizelink').attr('data-url');
+  "use strict";
+  var overlay = $(".djnjg-overlay"),
+    body = $("body"),
+    inner = overlay.find(".inner"),
+    visualizeUrl = $(".visualizelink").attr("data-url");
 
-    var disableArrowKeys = function (e) {
-        var ar = [37, 38, 39, 40];
-        if ($.inArray(e.keyCode, ar) >= 0) {
-            e.preventDefault();
-        }
-    };
+  var disableArrowKeys = function (e) {
+    var ar = [37, 38, 39, 40];
+    if ($.inArray(e.keyCode, ar) >= 0) {
+      e.preventDefault();
+    }
+  };
 
-    var openOverlay = function () {
-        // show overlay
-        $.get(visualizeUrl, function (html) {
-            overlay.show();
-            inner.html(html);
-            body.css('overflow', 'hidden');
-            inner.css('overflow', 'hidden');
+  var openOverlay = function () {
+    // show overlay
+    $.get(visualizeUrl, function (html) {
+      overlay.show();
+      inner.html(html);
+      body.css("overflow", "hidden");
+      inner.css("overflow", "hidden");
 
-            // Check if close button already exists
-            const closeBtn = document.querySelector('.closeBtn');
-            if (!closeBtn) {
-                const newcloseBtn = document.createElement('button');
-                newcloseBtn.setAttribute('class', 'closeBtn');
-                newcloseBtn.innerHTML = '&times;';
-                overlay.get(0).appendChild(newcloseBtn);
-            }
+      // Check if close button already exists
+      const closeBtn = document.querySelector(".closeBtn");
+      if (!closeBtn) {
+        const newcloseBtn = document.createElement("button");
+        newcloseBtn.setAttribute("class", "closeBtn");
+        newcloseBtn.innerHTML = "&times;";
+        overlay.get(0).appendChild(newcloseBtn);
+      }
 
-            window.graph = window.loadNetJsonGraph('.djnjg-overlay .inner');
+      window.graph = window.loadNetJsonGraph(".djnjg-overlay .inner");
 
-            overlay.find('.closeBtn').click(function (e) {
-                e.preventDefault();
-                closeOverlay();
-            });
-        });
-        $(document).keydown(disableArrowKeys);
-    };
-
-    var closeOverlay = function () {
-        // Make sure that whenever this function is called,
-        // We first destroy the echart instance
-        window.graph.echarts.dispose();
-        $(document).unbind('keydown', disableArrowKeys);
-        inner.empty();
-        overlay.hide();
-        body.attr('style', '');
-    };
-
-    $('.visualizelink').click(function (e) {
-        openOverlay();
+      overlay.find(".closeBtn").click(function (e) {
+        e.preventDefault();
+        closeOverlay();
+      });
     });
+    $(document).keydown(disableArrowKeys);
+  };
 
-    $(document).keyup(function (e) {
-        // ALT+P
-        if (e.altKey && e.which === 80) {
-            // unfocus any active input before proceeding
-            $(document.activeElement).trigger('blur');
-            // corresonding raw value before proceding
-            setTimeout(openOverlay, 15);
-        } else if (!e.ctrlKey && e.which === 27) {
-            // ESC
-            closeOverlay();
-        }
-    });
+  var closeOverlay = function () {
+    // Make sure that whenever this function is called,
+    // We first destroy the echart instance
+    window.graph.echarts.dispose();
+    $(document).unbind("keydown", disableArrowKeys);
+    inner.empty();
+    overlay.hide();
+    body.attr("style", "");
+  };
+
+  $(".visualizelink").click(function (e) {
+    openOverlay();
+  });
+
+  $(document).keyup(function (e) {
+    // ALT+P
+    if (e.altKey && e.which === 80) {
+      // unfocus any active input before proceeding
+      $(document.activeElement).trigger("blur");
+      // corresonding raw value before proceding
+      setTimeout(openOverlay, 15);
+    } else if (!e.ctrlKey && e.which === 27) {
+      // ESC
+      closeOverlay();
+    }
+  });
 });
