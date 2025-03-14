120c4c1e7fcc6213024cd236269d262d5ef21dd4
Federico Capoano <f.capoano@openwisp.io>
2025-03-14 15:43:50 -0300

    [qa] Reformatted JS/CSS with prettier

--- openwisp_network_topology/static/netjsongraph/js/strategy-switcher.js
+++ openwisp_network_topology/static/netjsongraph/js/strategy-switcher.js
@@ -1,18 +1,20 @@
 (function ($) {
-    'use strict';
-    $(document).ready(function () {
-        var strategy = $('#id_strategy'),
-            fetchRows = $('#id_url').parents('.form-row'),
-            receiveRows = $('#id_key, #id_expiration_time, #id_receive_url').parents('.form-row');
-        strategy.change(function (e) {
-            if (strategy.val() === 'fetch') {
-                fetchRows.show();
-                receiveRows.hide();
-            } else {
-                fetchRows.hide();
-                receiveRows.show();
-            }
-        });
-        strategy.trigger('change');
+  "use strict";
+  $(document).ready(function () {
+    var strategy = $("#id_strategy"),
+      fetchRows = $("#id_url").parents(".form-row"),
+      receiveRows = $("#id_key, #id_expiration_time, #id_receive_url").parents(
+        ".form-row",
+      );
+    strategy.change(function (e) {
+      if (strategy.val() === "fetch") {
+        fetchRows.show();
+        receiveRows.hide();
+      } else {
+        fetchRows.hide();
+        receiveRows.show();
+      }
     });
-}(django.jQuery));
+    strategy.trigger("change");
+  });
+})(django.jQuery);
